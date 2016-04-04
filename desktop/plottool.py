1e305d3a56ed7889c1def903aa175dc319e3da0e
Ludovic Legrand <ludovic.legrand@gmail.com>
2016-04-03 23:23:38 -0700

    Cleaned up plotting commands

--- desktop/plottool.py
+++ desktop/plottool.py
@@ -1,7 +1,4 @@
-#import matplotlib
-#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import StringIO
 
 def overplotgraph(angle,diff,BGpoly,Sum, graphlist,filename):
     fig = plt.figure(figsize=(15,5)) 
@@ -15,7 +12,7 @@ def overplotgraph(angle,diff,BGpoly,Sum, graphlist,filename):
     plt.plot(angle, Sum, linestyle="solid", color="green")
     plt.xlim(5,55)
     plt.ylim(0,max(diff)*2)
-    
+
     offset = max(diff)/2*3
     difference_magnification = 1
     difference = (diff - Sum) * difference_magnification
@@ -26,7 +23,6 @@ def overplotgraph(angle,diff,BGpoly,Sum, graphlist,filename):
     FOM = sum(abs(diff-Sum))/len(diff)
     plt.text(6, offset/10*12, filename, fontsize=12, color="black")
     #plt.text(6, offset/10*11, "Calculated with PatDB", fontsize=12, color="black")
-
     plt.text(50, offset/10*12, "FOM = %.2f" %(FOM), fontsize=12, color="red")
     vertpos = offset/10*9
     for i in range(0,len(graphlist)):
@@ -39,17 +35,6 @@ def overplotgraph(angle,diff,BGpoly,Sum, graphlist,filename):
     if len(graphlist) == 10:
         plt.text(6, vertpos,"...", fontsize=12, color="blue")
 
-    # buf = io.BytesIO()
-    # plt.savefig(buf, format='png')
-    # buf.seek(0)
-    # plot = Image.open(buf)
-    # buf.close()
-    # return plot
+    plt.show()
 
-    plt.show
-    
-    ludo_rv = StringIO.StringIO()
-    plt.savefig(ludo_rv, format="png")
-    #plt.clf()
-
-    return ludo_rv
+    return
